ea34803a5e5427960d78dc21ecc256983aaa1c54
sky172839465 <sky172839465@gmail.com>
2025-01-30 21:38:54 +0800

    style: ignore shadcn component

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/components/ui/*'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
