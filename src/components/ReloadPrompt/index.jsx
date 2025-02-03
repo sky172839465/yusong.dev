0ab5925473a0442f80514857576495cb69f9b460
sky172839465 <sky172839465@gmail.com>
2025-02-03 14:36:34 +0800

    feat: add downloading banner

--- src/components/ReloadPrompt/index.jsx
+++ src/components/ReloadPrompt/index.jsx
@@ -1,8 +1,10 @@
-import { MousePointerClick, X } from 'lucide-react'
+import { Download,MousePointerClick, X } from 'lucide-react'
+import { useState } from 'react'
 import { pwaInfo } from 'virtual:pwa-info'
 import { useRegisterSW } from 'virtual:pwa-register/react'
 
 console.log(pwaInfo)
+
 
 import { Alert, AlertDescription,AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
@@ -10,6 +12,7 @@ import { Button } from '@/components/ui/button'
 const INTERVAL_MS = 10 * 60 * 1000
 
 const ReloadPrompt = () => {
+  const [isUpdating, setIsUpdating] = useState(false)
   const {
     offlineReady: [offlineReady, setOfflineReady],
     needRefresh: [needRefresh, setNeedRefresh],
@@ -42,6 +45,7 @@ const ReloadPrompt = () => {
       return
     }
 
+    setIsUpdating(true)
     updateServiceWorker(true)
   }
 
@@ -49,28 +53,56 @@ const ReloadPrompt = () => {
     (offlineReady || needRefresh) && (
       <div className='fixed bottom-4 left-1/2 z-50 w-full max-w-sm -translate-x-1/2'>
         <Alert className='flex cursor-pointer items-center justify-between gap-2 rounded-xl border border-foreground bg-background/50 p-4 text-foreground shadow-xl backdrop-blur-md'>
-          <div
-            onClick={opUpdate}
-          >
-            <AlertTitle className='flex items-center gap-2'>
-              {!offlineReady && (
-                <MousePointerClick className='size-5' />
-              )}
-              <span>
-                {offlineReady ? 'Offline Ready' : 'Update Available'}
-              </span>
-            </AlertTitle>
-            <AlertDescription>
-              {offlineReady
-                ? 'App is ready to work offline.'
-                : 'New content available, click to update.'}
-            </AlertDescription>
-          </div>
-          <div className='flex'>
-            <Button size='icon' variant='ghost' onClick={onClose}>
-              <X className='size-4' />
-            </Button>
-          </div>
+          {!isUpdating && (
+            <>
+              <div
+                onClick={opUpdate}
+              >
+                <AlertTitle className='flex items-center gap-2'>
+                  {!offlineReady && (
+                    <MousePointerClick className='size-5' />
+                  )}
+                  <span>
+                    {offlineReady ? 'Offline Ready' : 'Update Available'}
+                  </span>
+                </AlertTitle>
+                <AlertDescription>
+                  {offlineReady
+                    ? 'App is ready to work offline.'
+                    : 'New content available, click to update.'}
+                </AlertDescription>
+              </div>
+              <div className='flex'>
+                <Button size='icon' variant='ghost' onClick={onClose}>
+                  <X className='size-4' />
+                </Button>
+              </div>
+            </>
+          )}
+          {isUpdating && (
+            <div>
+              <AlertTitle className='flex items-center gap-2'>
+                {!offlineReady && (
+                  <Download className='size-5' />
+                )}
+                <span>
+                  Downloading
+                </span>
+              </AlertTitle>
+              <AlertDescription>
+                {'Please waiting for new content downloaded.'.split(' ').map((word, index) => {
+                  return (
+                    <span
+                      key={index}
+                      className='animate-pulse font-medium'
+                    >
+                      {`${word} `}
+                    </span>
+                  )
+                })}
+              </AlertDescription>
+            </div>
+          )}
         </Alert>
       </div>
     )
