197349842f7f5565c274e4920bca61f0cfb90e22
sky172839465 <sky172839465@gmail.com>
2025-02-03 13:41:46 +0800

    feat: skip waiting by updateServiceWorker

--- public/sw.js
+++ public/sw.js
@@ -2,21 +2,28 @@ import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate } from 'workbox-strategies'
 
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
+
+
 cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST)
 
 // Activate new service worker immediately
-self.addEventListener('install', (event) => {
-  console.log('[SW] Installed', event)
-  self.skipWaiting()
-})
+// self.addEventListener('install', (event) => {
+//   console.log('[SW] Installed', event)
+//   self.skipWaiting()
+// })
 
 // Claim clients immediately after activation
-self.addEventListener('activate', (event) => {
-  console.log('[SW] Activated', event)
-  event.waitUntil(self.clients.claim())
-})
+// self.addEventListener('activate', (event) => {
+//   console.log('[SW] Activated', event)
+//   event.waitUntil(self.clients.claim())
+// })
 
 registerRoute(
   ({ request }) => request.destination === 'script' || request.destination === 'style',
