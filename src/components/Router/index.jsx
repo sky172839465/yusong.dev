d3b33c732e104782d75a64abace5960df8157d06
Yusong Hsu <sky172839465@gmail.com>
2025-02-04 23:17:38 +0800

    style: decrease page navigate animate time (#125)

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -34,7 +34,7 @@ const withErrorElement = (routes) => routes.map((item) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 0.3 }}
         >
           <Suspense
             fallback={(
