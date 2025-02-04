e5624d81684a96c21e032145e79dc755f7eba7b7
sky172839465 <sky172839465@gmail.com>
2025-02-04 09:52:41 +0800

    chore: generate og into png

--- cloudflare-workers/seo/src/index.js
+++ cloudflare-workers/seo/src/index.js
@@ -12,8 +12,8 @@ const ROUTE_MAP = keyBy(
     const imageFolder = `${IMG_HOST}/${file.replace(/index.jsx|index.md/, 'images')}`
     return {
       ...item,
-      image: `${imageFolder}/og.svg`,
-      twitterImage: `${imageFolder}/x.svg`
+      image: `${imageFolder}/og.png`,
+      twitterImage: `${imageFolder}/x.png`
     }
   }),
   'path'
--- scripts/generateOgImageData.jsx
+++ scripts/generateOgImageData.jsx
@@ -4,6 +4,7 @@ import { tryit } from 'radash'
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import satori from 'satori'
+import sharp from 'sharp'
 import { twj } from 'tw-to-css'
 
 const routes = JSON.parse(fs.readFileSync('src/data/routes.json', 'utf-8'))
@@ -77,12 +78,19 @@ const IMAGE_TYPE = {
   X: 'x'
 }
 
-const IMAGE_FILE = {
+const IMAGE_SVG_FILE = {
   [IMAGE_TYPE.OG]: 'og.svg',
   [IMAGE_TYPE.X]: 'x.svg'
 }
 
+
+const IMAGE_PNG_FILE = {
+  [IMAGE_TYPE.OG]: 'og.png',
+  [IMAGE_TYPE.X]: 'x.png'
+}
+
  
+// eslint-disable-next-line react-refresh/only-export-components
 const DIMENSIONS = {
   [IMAGE_TYPE.OG]: { width: 1200, height: 630 },
   [IMAGE_TYPE.X]: { width: 1200, height: 675 }
@@ -92,8 +100,10 @@ const fontData = fs.readFileSync('scripts/fonts/NotoSansTC-Regular.ttf')
 
 const generateSVG = async (route, imageType) => {
   const { file } = route
-  const imageFile = IMAGE_FILE[imageType]
-  const ogImgPath = file.replace(/index.jsx|index.md/, `images/${imageFile}`)
+  const imageSvgFile = IMAGE_SVG_FILE[imageType]
+  const imagePngFile = IMAGE_PNG_FILE[imageType]
+  const ogSvgImgPath = file.replace(/index.jsx|index.md/, `images/${imageSvgFile}`)
+  const ogPngImgPath = file.replace(/index.jsx|index.md/, `images/${imagePngFile}`)
   const Component = getOgImgComponent(route)
   const svg = await satori(Component, {
     fonts: [
@@ -106,18 +116,20 @@ const generateSVG = async (route, imageType) => {
     ...DIMENSIONS[imageType]
   })
 
-  const outputDir = ogImgPath.replace(imageFile, '')
+  const outputDir = ogPngImgPath.replace(imagePngFile, '')
   const [error] = await tryit(() => fs.promises.access(outputDir))()
   if (error) {
     await fs.promises.mkdir(outputDir, { recursive: true })
   }
   
-  return fs.promises.writeFile(ogImgPath, svg)
+  // await fs.promises.writeFile(ogSvgImgPath, svg)
+  // const svgContent = await fs.promises.readFile(ogSvgImgPath, 'utf-8')
+  return sharp(Buffer.from(svg)).png().toFile(ogSvgImgPath).then(console.log)
 }
 
 const [error] = await tryit(() => {
-  return Promise.all(concat(...[IMAGE_TYPE.OG, IMAGE_TYPE.X].map((imageType) => {
-    return routes.map((route) => generateSVG(route, imageType))
+  return Promise.all(concat(...[IMAGE_TYPE.OG].map((imageType) => {
+    return [routes[0]].map((route) => generateSVG(route, imageType))
   })))
 })()
 
