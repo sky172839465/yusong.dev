d91160839583814be866d98a538cabd03f0ae8c6
sky172839465 <sky172839465@gmail.com>
2025-02-05 20:36:24 +0800

    fix: srcset notworking

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -68,7 +68,6 @@ const useArticleHtml = (html, pageImages) => {
             width="${width}"
             height="${height}"
             data-loaded="false"
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 1200px'
             onload="this.setAttribute('data-loaded', 'true'); this.previousElementSibling.setAttribute('data-visible', 'false');"
             class="w-full rounded-lg relative [&[data-loaded='false']]:invisible"
             loading="lazy"
@@ -121,7 +120,6 @@ const Article = (props) => {
         </h1>
         <LazyImage
           imageData={mainImageData}
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 1200px'
           alt={title}
           className={`aspect-video w-full rounded-lg ${isEmpty(mainImageData) && 'my-8'}`}
           isLoading={isLoading}
