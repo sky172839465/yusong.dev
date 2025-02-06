3b9ad8442e694a87afcfdd8c77715d790bce9d87
sky172839465 <sky172839465@gmail.com>
2025-02-06 10:23:32 +0800

    test

--- scripts/updateArticleFrontmatter.js
+++ scripts/updateArticleFrontmatter.js
@@ -3,9 +3,10 @@ import yaml from 'js-yaml'
 import path from 'path'
 import { simpleGit } from 'simple-git'
 
-(async function () {
-  const { stdout = '' } = await simpleGit().raw(['diff', '--name-status', 'origin/main'])
-  console.log(stdout)
+const updateArticleFrontmatter = async () => {
+  const diff = await simpleGit().diff(['--name-status', 'main'])
+  console.log({ diff })
+  const stdout = ''
   const lines = stdout.trim().split('\n')
 
   const modifiedMarkdownFiles = lines
@@ -14,22 +15,33 @@ import { simpleGit } from 'simple-git'
     .map(([, file]) => file)
 
   const today = new Date().toISOString().split('T')[0]
+  console.log(today, modifiedMarkdownFiles)
 
-  modifiedMarkdownFiles.forEach(file => {
+  await modifiedMarkdownFiles.map(async (file) => {
     const filePath = path.join(process.cwd(), file)
-    if (!fs.existsSync(filePath)) return
+    if (!fs.existsSync(filePath)) {
+      return
+    }
 
-    let content = fs.readFileSync(filePath, 'utf8')
+    let content = await fs.promises.readFile(filePath, 'utf8')
     const match = content.match(/^---\n([\s\S]+?)\n---/)
 
-    if (match) {
-      let frontmatter = yaml.load(match[1])
-      if (frontmatter.modifiedAt) {
-        frontmatter.modifiedAt = today
-        const updatedFrontmatter = `---\n${yaml.dump(frontmatter)}---`
-        content = content.replace(match[0], updatedFrontmatter)
-        fs.writeFileSync(filePath, content, 'utf8')
-      }
+    if (!match) {
+      return
     }
+
+    let frontmatter = yaml.load(match[1])
+    if (!frontmatter.modifiedAt) {
+      return
+    }
+
+    frontmatter.modifiedAt = today
+    const updatedFrontmatter = `---\n${yaml.dump(frontmatter)}---`
+    content = content.replace(match[0], updatedFrontmatter)
+    return fs.promises.writeFile(filePath, content, 'utf8')
   })
-})()
+
+  process.exit(0)
+}
+
+updateArticleFrontmatter()
