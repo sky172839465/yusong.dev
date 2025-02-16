e51e65dc65d009110bb6d50069ec40d5b4e4cca3
sky172839465 <sky172839465@gmail.com>
2025-02-16 18:49:37 +0800

    feat: default hide giscus

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,24 +1,67 @@
-import Giscus from '@giscus/react'
+import { ChevronsDownUp, ChevronsUpDown } from 'lucide-react'
+import { lazy, Suspense } from 'react'
+import { useLocalStorage } from 'usehooks-ts'
 
+import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import useTheme from '@/hooks/useTheme'
+
+import FadeIn from '../FadeIn'
+
+const COMMENTS_VISIBLE_KEY = 'COMMENTS_VISIBLE_KEY'
+
+const LazyGiscus = lazy(() => import('@giscus/react'))
+
+const GiscusSkeleton = () => {
+  return (
+    <Skeleton className='h-[30dvh] w-full' />
+  )
+}
 
 const Comments = () => {
   const { isDarkMode } = useTheme()
+  const [visible, setVisible] = useLocalStorage(COMMENTS_VISIBLE_KEY, false)
+
+  const toggleVisible = () => setVisible(!visible)
+
   return (
-    <Giscus
-      id='comments'
-      repo='sky172839465/yusong.tw'
-      repoId='R_kgDONva8Tw'
-      category='Announcements'
-      categoryId='DIC_kwDONva8T84Cmdvk'
-      mapping='pathname'
-      reactionsEnabled='1'
-      emitMetadata='0'
-      inputPosition='top'
-      theme={isDarkMode ? 'dark' : 'light'}
-      lang='zh-TW'
-      loading='lazy'
-    />
+    <>
+      <Button className='my-4 p-6' onClick={toggleVisible}>
+        {visible && (
+          <>
+            <ChevronsDownUp className='size-4' />
+            Hide comments
+          </>
+        )}
+        {!visible && (
+          <>
+            <ChevronsUpDown className='size-4' />
+            Show comments
+          </>
+        )}
+        
+      </Button>
+      {visible && (
+        <Suspense fallback={<GiscusSkeleton />}>
+          <FadeIn>
+            <LazyGiscus
+              id='comments'
+              repo='sky172839465/yusong.tw'
+              repoId='R_kgDONva8Tw'
+              category='Announcements'
+              categoryId='DIC_kwDONva8T84Cmdvk'
+              mapping='pathname'
+              reactionsEnabled='1'
+              emitMetadata='0'
+              inputPosition='top'
+              theme={isDarkMode ? 'dark' : 'light'}
+              lang='zh-TW'
+              loading='lazy'
+            />
+          </FadeIn>
+        </Suspense>
+      )}
+    </>
   )
 }
 
