b5d8598de25f3d86c24a11260e7d3494262dd5f1
Yusong Hsu <sky172839465@gmail.com>
2025-02-19 23:14:43 +0800

    feat: add en pwa index meta

---
+++ src/pages/en/article/pwa/index.meta.js
@@ -0,0 +1,5 @@
+const meta = {
+  title: 'Progressive Web App (PWA) Series',
+  description: 'It can run like an app and even function normally without an internet connection—these are the characteristics of a Progressive Web App (PWA)!'
+}
+export default meta
