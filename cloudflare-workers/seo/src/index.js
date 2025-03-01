c0030f692bc2ebca2bd22e7e5e79e4c90d6eabd1
Yusong Hsu <sky172839465@gmail.com>
2025-03-02 01:05:11 +0800

    fix: nojs page 500 error (#260)
    * fix: nojs page 500
    
    * Update deploy-cf-worker.yml
    
    * Update index.js
    
    * Update index.js
    
    * Update deploy-cf-worker.yml
    
    * Update wrangler.toml

--- cloudflare-workers/seo/src/index.js
+++ cloudflare-workers/seo/src/index.js
@@ -41,14 +41,13 @@ export default {
     }
 
     // Fetch the original HTML
-    let [html, noJsHtml] = await Promise.all([
+    let [html, jsHtml] = await Promise.all([
       fetch(request).then((response) => response.text()),
-      isNoJsRoute ? await fetch(path).then((response) => response.text()) : Promise.resolve()
+      isNoJsRoute ? await fetch(`https://${BLOG_HOST}${convertedPath}`).then((response) => response.text()) : Promise.resolve()
     ])
-    console.log(targetRoute)
 
     if (isNoJsRoute) {
-      html = html.replace(/<body[^>]*>([\s\S]*)<\/body>/, noJsHtml)
+      html = jsHtml.replace(/<body[^>]*>([\s\S]*)<\/body>/, html)
     }
 
     // Dynamic generate meta tags
@@ -57,7 +56,7 @@ export default {
     const displayTitle = `${title}${title === TITLE ? '' : ` | ${TITLE}`}`
     const modifiedHtml = html.replace(
       '</head>',
-      `   ${(isArticle && !isNoJsRoute) ? `<noscript><meta http-equiv="refresh" content="0;url=${NO_JS_PATH}${path}index.html"></noscript>` : ''}
+      `   ${(isArticle && !isNoJsRoute) ? `<noscript><meta http-equiv="refresh" content="0;url=${NO_JS_PATH}${path}"></noscript>` : ''}
           <link rel="preconnect" href="${CDN_HOST}" crossorigin />
           <link rel="dns-prefetch" href="${CDN_HOST.replace('https:', '')}" />
           <link rel="canonical" href="${requestUrl}"/>
