6eba3bcd9dc8bd3abba14a458cf202cc18d698cf
Yusong Hsu <sky172839465@gmail.com>
2025-03-02 11:28:57 +0800

    Update getConvertedHtml.js

--- src/utils/getConvertedHtml.js
+++ src/utils/getConvertedHtml.js
@@ -90,7 +90,7 @@ const getConvertedHtml = async (originHtml, fileFolder, label = {}) => {
       return codeToHtml(code, {
         lang,
         themes: { light: 'github-light', dark: 'github-dark' },
-        defaultColor: isEmpty(label) ? true : false
+        defaultColor: isEmpty(label) ? 'light' : false
       }).then((highlight) => ({ lang, code, highlight }))
     })
   )
