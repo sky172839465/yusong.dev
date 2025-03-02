be0de3372bcd146f1f0507b18a15f0cd3f7c0fb5
Yusong Hsu <sky172839465@gmail.com>
2025-03-02 23:48:56 +0800

    style: no image default add aspect-video

--- src/components/LazyImage/index.jsx
+++ src/components/LazyImage/index.jsx
@@ -41,7 +41,7 @@ const ImageStatus = (props) => {
 
   if (isEmpty(src)) {
     return (
-      <Skeleton className={`${className ? className : ''} flex items-center justify-center text-4xl text-foreground`}>
+      <Skeleton className={`${className ? className : ''} aspect-video flex items-center justify-center text-4xl text-foreground`}>
         <p>
           NO IMAGE
         </p>
