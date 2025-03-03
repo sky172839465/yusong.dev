0b7c8110d862d855d75e479b0486e88b64107dba
Yusong Hsu <sky172839465@gmail.com>
2025-03-03 15:07:03 +0800

    style: decrease transition timing (#296)

--- src/components/Router/index.jsx
+++ src/components/Router/index.jsx
@@ -35,7 +35,7 @@ const withErrorElement = (routes) => routes.map((item) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.8 }}
+          transition={{ duration: 0.6 }}
         >
           <Suspense
             fallback={(
