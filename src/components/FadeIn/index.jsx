2d3750936d25da1fd8aecaf3dd4fa9e2b7fff069
Yusong Hsu <sky172839465@gmail.com>
2025-03-03 14:05:43 +0800

    Update index.jsx

--- src/components/FadeIn/index.jsx
+++ src/components/FadeIn/index.jsx
@@ -5,7 +5,7 @@ const FadeIn = (props) => {
     <m.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.5 }}
       {...props}
     />
   )
