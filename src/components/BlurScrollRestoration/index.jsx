0e8132f5b01009116f532e1d1f33881f39514123
Yusong Hsu <sky172839465@gmail.com>
2025-03-08 17:49:04 +0800

    chore: remove state (#319)

--- src/components/BlurScrollRestoration/index.jsx
+++ src/components/BlurScrollRestoration/index.jsx
@@ -1,39 +1,25 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { useLocation, useNavigation } from 'react-router-dom'
 
 const useScrollRestoration = () => {
   const { pathname } = useLocation()
   const navigation = useNavigation()
-  const [loading, setLoading] = useState(false)
-  const [done, setDone] = useState(true)
   const timer = useRef()
 
   useEffect(() => {
-    if (navigation.state === 'loading') {
-      setLoading(true)
-      setDone(false)
-      setTimeout(() => {
-        window.scrollTo({ top: 0, behavior: 'auto' })
-      }, 100)
-    }
-    
     if (navigation.state !== 'idle') {
       return
     }
 
     clearTimeout(timer.current)
     timer.current = setTimeout(() => {
-      setLoading(false)
-      setTimeout(() => setDone(true), 150)
-    }, 200)
+      window.scrollTo({ top: 0, behavior: 'auto' })
+    }, 100)
     return () => clearTimeout(timer.current)
   }, [pathname, navigation])
-
-  return { loading, done }
 }
 
 const BlurScrollRestoration = () => {
-  // const { loading, done } = useScrollRestoration()
   useScrollRestoration()
 
   return null
