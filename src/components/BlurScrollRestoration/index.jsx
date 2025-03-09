01570333715545e36917ba4eb2cc6b34f46104ab
Yusong Hsu <sky172839465@gmail.com>
2025-03-10 02:34:29 +0800

    Update index.jsx

--- src/components/BlurScrollRestoration/index.jsx
+++ src/components/BlurScrollRestoration/index.jsx
@@ -1,28 +1,38 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigation } from 'react-router-dom'
 
 const useScrollRestoration = () => {
   const { pathname } = useLocation()
   const navigation = useNavigation()
+  const [loading, setLoading] = useState(false)
   const timer = useRef()
 
   useEffect(() => {
     if (navigation.state === 'loading') {
       return
     }
-    
+
+    setLoading(true)
+    window.scrollTo({ top: 0, behavior: 'auto' })
     clearTimeout(timer.current)
     timer.current = setTimeout(() => {
-      window.scrollTo({ top: 0, behavior: 'auto' })
+      setLoading(false)
     }, 150)
     return () => clearTimeout(timer.current)
   }, [pathname, navigation])
+
+  return { loading }
 }
 
-const BlurScrollRestoration = () => {
-  useScrollRestoration()
+const BlurScrollRestoration = (props) => {
+  const { children } = props
+  const { loading } = useScrollRestoration()
 
-  return null
+  if (loading) {
+    return null
+  }
+
+  return children
 }
 
 export default BlurScrollRestoration
