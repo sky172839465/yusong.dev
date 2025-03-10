97da8b2cdea39a80a039556803301ca24854c8b3
Yusong Hsu <sky172839465@gmail.com>
2025-03-10 12:55:32 +0800

    reverse: remove pop check (#363)

--- src/components/BlurScrollRestoration/index.jsx
+++ src/components/BlurScrollRestoration/index.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from 'react'
-import { useLocation, useNavigation, useNavigationType } from 'react-router-dom'
+import { useLocation, useNavigation } from 'react-router-dom'
 
 import SkeletonHome from '@/components/SkeletonHome'
 
 const useScrollRestoration = () => {
   const { pathname } = useLocation()
   const navigation = useNavigation()
-  const navigationType = useNavigationType()
   const [loading, setLoading] = useState(false)
   const timer = useRef()
 
@@ -22,11 +21,6 @@ const useScrollRestoration = () => {
   }, [pathname])
 
   useEffect(() => {
-    if (navigationType === 'POP') {
-      setLoading(false)
-      return
-    }
-
     setLoading(true)
     clearTimeout(timer.current)
     timer.current = setTimeout(() => {
@@ -34,7 +28,7 @@ const useScrollRestoration = () => {
       setTimeout(() => setLoading(false), 150)
     }, 100)
     return () => clearTimeout(timer.current)
-  }, [pathname, navigation, navigationType])
+  }, [pathname, navigation])
 
   return { loading, setLoading }
 }
