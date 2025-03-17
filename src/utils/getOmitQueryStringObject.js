6d9c8793631eb059e0e01e76f2e49a4577fdff28
Yusong Hsu <sky172839465@gmail.com>
2025-03-18 03:18:05 +0800

    fix: undefined error

--- src/utils/getOmitQueryStringObject.js
+++ src/utils/getOmitQueryStringObject.js
@@ -2,7 +2,7 @@ import queryString from 'query-string'
 
 import getOmitObject from '@/utils/getOmitObject'
 
-const getOmitQueryStringObject = (qsString, searchParams) => {
+const getOmitQueryStringObject = (qsString, searchParams = '') => {
   const qsObj = queryString.parse(qsString || searchParams.toString(), { arrayFormat: 'repeat' })
   return getOmitObject(qsObj)
 }
