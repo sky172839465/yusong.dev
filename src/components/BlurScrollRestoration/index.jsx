fe44b8129c97481226bd1b6912500d6e090816b3
Yusong Hsu <sky172839465@gmail.com>
2025-03-19 04:54:12 +0800

    chore: decrease timing (#404)

--- src/components/BlurScrollRestoration/index.jsx
+++ src/components/BlurScrollRestoration/index.jsx
@@ -26,8 +26,8 @@ const useScrollRestoration = () => {
     clearTimeout(timer.current)
     timer.current = setTimeout(() => {
       window.scrollTo({ top: 0, behavior: 'auto' })
-      setTimeout(() => setLoading(false), 100)
-    }, 100)
+      setTimeout(() => setLoading(false), 50)
+    }, 50)
     return () => clearTimeout(timer.current)
   }, [pathname, navigation])
 
