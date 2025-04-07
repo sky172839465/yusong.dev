da6220ab54c55d882895a21df56d2d6c485f42db
Yusong Hsu <sky172839465@gmail.com>
2025-04-07 12:33:41 +0800

    chore: prevent rest lazy image props (#470)

--- src/components/LazyImage/Dialog/index.jsx
+++ src/components/LazyImage/Dialog/index.jsx
@@ -19,16 +19,14 @@ const i18nMapping = {
 const LazyImagePreview = (props) => {
   const { label } = useI18N(i18nMapping)
   const [open, setOpen] = useState(false)
-  const { className, imageData, alt, ...restProps } = props
+  const { className, imageData, alt } = props
   const { webp, width, height } = get(imageData, 'original', {})
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger className='w-full cursor-pointer'>
         <LazyImage
+          {...props}
           className={`${className || ''} transition md:hover:scale-105`}
-          imageData={imageData}
-          alt={alt}
-          {...restProps}
         />
       </DialogTrigger>
       <DialogContent className='max-w-[94dvw] border-none bg-transparent p-0 text-white shadow-none md:max-w-[80dvw]'>
