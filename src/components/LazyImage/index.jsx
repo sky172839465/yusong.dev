986f3e2d3a132a02916915271a8a8fd2460a0b1c
Yusong Hsu <sky172839465@gmail.com>
2025-04-07 14:41:48 +0800

    style: remove unused (#479)

--- src/components/LazyImage/index.jsx
+++ src/components/LazyImage/index.jsx
@@ -1,4 +1,4 @@
-import { isEmpty, isUndefined, omit } from 'lodash-es'
+import { isEmpty, omit } from 'lodash-es'
 import { AlertCircle } from 'lucide-react'
 import { AnimatePresence } from 'motion/react'
 import * as m from 'motion/react-m'
@@ -119,24 +119,17 @@ const LazyImage = (props) => {
             />
           </m.div>
         )}
-        {isUndefined(src) && (
-          <m.div
-            className='my-8 aspect-video w-full'
-            exit={{ opacity: 0 }}
-          />
-        )}
-        {!isUndefined(src) && (
-          <m.img
-            onLoad={onLoad}
-            onError={onError}
-            className={`${isLoaded ? '' : 'invisible'} ${className}`}
-            loading={loading}
-            initial={{ opacity: 0, filter: 'blur(10px)' }}
-            animate={{ opacity: isLoaded ? 1 : 0, filter: isLoaded ? 'blur(0px)' : 'blur(10px)' }}
-            transition={{ duration: 0.5 }}
-            {...imageAttributes}
-          />
-        )}
+        <m.img
+          onLoad={onLoad}
+          onError={onError}
+          // className={`${isLoaded ? '' : 'invisible'} ${className}`}
+          className={className}
+          loading={loading}
+          initial={{ opacity: 0, filter: 'blur(10px)' }}
+          animate={{ opacity: (isLoaded && !error) ? 1 : 0, filter: isLoaded ? 'blur(0px)' : 'blur(10px)' }}
+          transition={{ duration: 0.5 }}
+          {...imageAttributes}
+        />
       </FadeIn>
     </AnimatePresence>
   )
