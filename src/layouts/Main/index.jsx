75f4fdb1da377c350e83cacdcd13a3ddebae520d
Yusong Hsu <sky172839465@gmail.com>
2025-04-09 00:55:59 +0800

    Update index.jsx

--- src/layouts/Main/index.jsx
+++ src/layouts/Main/index.jsx
@@ -1,16 +1,19 @@
 import { lazy } from 'react'
 
 import FadeIn from '@/components/FadeIn'
+import { usePageLoading } from '@/contexts/pageLoading'
 
 const LazyHeader = lazy(() => import('../Header'))
 const LazyFooter = lazy(() => import('../Footer'))
 
 const MainLayout = (props) => {
   const { children, isFullScreen } = props
+  const { loading } = usePageLoading()
+
   return (
     <FadeIn className='flex min-h-dvh flex-col bg-background'>
       <LazyHeader />
-      <main className={`container mx-auto grow px-4 py-8 ${isFullScreen && 'flex items-center'}`}>
+      <main className={`container mx-auto grow px-4 py-8 ${isFullScreen ? 'flex items-center' : ''} opacity-100 transition-opacity ${loading ? 'invisible opacity-0' : 'delay-150'}`}>
         {children}
       </main>
       <LazyFooter />
