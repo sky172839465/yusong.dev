968f49976141d8e761c04176d0d825b3900198b8
sky172839465 <9082423+sky172839465@users.noreply.github.com>
2025-10-11 07:16:14 +0000

    docs: generate article [2025 JP 旅行前紀錄]

---
+++ src/pages/article/jp2025/index.jsx
@@ -0,0 +1,13 @@
+import ArticleIndex from '@/components/ArticleIndex'
+
+const Index = () => {
+  return (
+    <ArticleIndex>
+      <div>
+        testing description
+      </div>
+    </ArticleIndex>
+  )
+}
+
+export default Index
---
+++ src/pages/article/jp2025/index.meta.js
@@ -0,0 +1,5 @@
+const meta = {
+  title: '測試',
+  description: '測試系列文章'
+}
+export default meta
