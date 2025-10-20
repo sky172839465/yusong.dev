2f39ff00a350405d4d157fba5de997de48f8a286
sky172839465 <sky172839465@gmail.com>
2025-10-20 20:01:02 +0800

    feat: support responsive size

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -25,6 +25,7 @@ const buttonVariants = cva(
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
+        responsive: "h-9 w-9 md:h-9 md:px-4 md:py-2 md:w-auto"
       },
     },
     defaultVariants: {
